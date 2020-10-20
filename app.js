137767dc7549da3ff92d65d1a356993da4f88423
Luis Vallejo <luiavag@gmail.com>
2020-10-19 22:57:14 -0500

    new users can create accounts and exiting users can login

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 // Constants
 let _CURRENT_USER
 const LS_SESSION_NAME = 'todos.sessionToken'
+const LS_USERS_PREFIX = 'todos.user_'
 const _SESSION = JSON.parse(localStorage.getItem(LS_SESSION_NAME)) || null
 
 // Selectors
-const navLiElems =  document.querySelectorAll('nav ul li')
+const navLiElems =  document.querySelectorAll('nav ul li a')
 const loggedOutNavLinks = document.querySelectorAll('[data-logged-out]')
 const loggedInNavLinks = document.querySelectorAll('[data-logged-in]')
 
@@ -37,7 +38,7 @@ signUpBtns.forEach(button => {
         showSignUpSection()
 
         // Set the 'active' class
-        activeNavBtn(e.target)
+        activeNavBtn('signup')
     })
 })
 
@@ -53,7 +54,7 @@ logInBtns.forEach(button => {
         showLogInSection()
 
         // Set the 'active' class
-        activeNavBtn(e.target)
+        activeNavBtn('login')
     })
 })
 
@@ -63,6 +64,34 @@ logOutBtnNav.addEventListener('click', (e) => {
 
     // Log the user out
     logUserOut()
+})
+
+// Dashboard button
+dashboardBtnNav.addEventListener('click', (e) => {
+    e.preventDefault()
+
+    // Hide all sections
+    hideAllSections()
+
+    // Load the sectionLogIn
+    showDashboardSection()
+
+    // Set the 'active' class
+    activeNavBtn('dashboard')
+})
+
+// Account button
+accountBtnNav.addEventListener('click', (e) => {
+    e.preventDefault()
+
+    // Hide all sections
+    hideAllSections()
+
+    // Load the sectionLogIn
+    showAccountSection()
+
+    // Set the 'active' class
+    activeNavBtn('account')
 })
 
 // SignUp form
@@ -79,6 +108,9 @@ function formsHandler(e) {
     // Prevent the form from submiting
     e.preventDefault()
 
+    // Get the form ID
+    const formId = e.target.id
+
     // Turn the inputs into a payload
     const payload = {}
 
@@ -90,82 +122,197 @@ function formsHandler(e) {
         const input = inputs[i];
         if (input.type !== 'submit') {
             // Build the payload
-            payload[input.name] = input.type == 'checkbox' ? input.checked : input.value
+            payload[input.name] = input.type == 'checkbox' ? input.checked : input.value.trim()
         }
     }
 
     // @TODO
-    // Validate payload
-    // validatePayload(payload)
+    // Validate payload & throw err accordingly
+    if (!validatePayload(payload, formId)) {
+        // @TODO - Throw error message to user
+        console.log(formId, 'Missing or invalid field(s) supplied')
+        return
+    }
 
-    console.log('payload', payload)
+    // Assign the current user
+    _CURRENT_USER = payload.email
+
+    // Sign Up Form: Check if the user is unique
+    if (formId == 'signup-form' && isUserUnique(_CURRENT_USER)) {
+        // Hash the user's password
+        payload.password = hashedPassword(payload.password)
+
+        // Store the payload
+        localStorage.setItem(LS_USERS_PREFIX + _CURRENT_USER, JSON.stringify(payload))
+
+        // Create a session
+        const sessionData = {
+            id: Date.now(),
+            user: _CURRENT_USER
+        }
+        localStorage.setItem(LS_SESSION_NAME, JSON.stringify(sessionData))
+
+        // Log the user in and redirect
+        logUserIn()
+
+        // Activate nav dashboard link
+        activeNavBtn('dashboard')
+
+        // TODO - remove log
+        console.log('signup-form >>>', 'All good')
+    }
+
+    // Log In Form
+    if (formId == 'login-form') {
+
+        // Get the user object
+        const user = JSON.parse(localStorage.getItem(LS_USERS_PREFIX + _CURRENT_USER)) || null
+
+        // Get the hashed password from the user's object or default to an empty string
+        const hash = typeof(user) == 'object' && user !== null ? user.password : ''
+
+        // Verify the user's password and continue or throw an error
+        if (verifyPassword(payload.password, hash)) {
+
+            // Create a session
+            const sessionData = {
+                id: Date.now(),
+                user: _CURRENT_USER
+            }
+            localStorage.setItem(LS_SESSION_NAME, JSON.stringify(sessionData))
+
+            // Log the user in and redirect
+            logUserIn()
+
+            // Activate nav dashboard link
+            activeNavBtn('dashboard')
+
+            // TODO - remove log
+            console.log('login-form >>>', 'All good')
+        } else {
+
+            // TODO - remove log
+            console.log('login-form >>>', 'Incorrect password or the user may not exists')
+        }
+    }
+
+    // @TODO - Reset the form
+    e.target.reset()
 }
 
-function validatePayload(payload) {
+function validatePayload(payload, formId) {
     payload = typeof(payload) == 'object' && payload !== null ? payload : false
+    formId = typeof(formId) == 'string' && formId.trim().length > 0 ? formId.trim() : false
 
-    if (payload)
+    // console.log('validatePayload() -> formId >>>', formId)
+    // console.log('validatePayload() -> payload >>>', payload)
+
+    if (payload && formId == 'signup-form') {
+        return (typeof(payload['firstName']) == 'string' && payload['firstName'].length > 0) && 
+            (typeof(payload['lastName']) == 'string' && payload['lastName'].length > 0) && 
+            (typeof(payload['email']) == 'string' && validateEmail(payload['email'])) && 
+            (typeof(payload['password']) == 'string' && payload['password'].length >= 4) && 
+            (typeof(payload['tosAgreement']) == 'boolean' && payload['tosAgreement'] !== false)
+    }
+    
+    if (payload && formId == 'login-form') {
+        return (typeof(payload['email']) == 'string' && validateEmail(payload['email'])) && 
+            (typeof(payload['password']) == 'string' && payload['password'].length >= 4)
+    }
+    
+    if (payload && formId == 'account-form') {
+        return (typeof(payload['firstName']) == 'string' && payload['firstName'].length > 0) && 
+            (typeof(payload['lastName']) == 'string' && payload['lastName'].length > 0) && 
+            (typeof(payload['email']) == 'string' && validateEmail(payload['email'])) && 
+            (typeof(payload['password']) == 'string' && payload['password'].length >= 4)
+    } 
+
+    // Default to false
+    return false
+    
 }
 
-function activeNavBtn(e) {
+function validateEmail(email) {
+    // Validate parameters
+    email = typeof(email) == 'string' && email.length >= 6 ? email : false;
+    const regEx = /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,4})(\]?)$/;
+
+    if (email) {
+        return regEx.test(email)
+    } else {
+        return false
+    }
+}
+
+function isUserUnique(user) {
+    if (localStorage[LS_USERS_PREFIX + user] == undefined) {
+        // @TODO - habdle error & remove log
+        console.log('signup-form >>>', 'Success: user is unique')
+        return true
+    }
+
+    // @TODO - habdle error & remove log
+    console.log('signup-form >>>', 'Error: A user with that email address already exists')
+    return false
+} 
+
+function hashedPassword(password) {
+    // Require bcrypt
+    const bcrypt = dcodeIO.bcrypt;
+
+    // Auto-gen a salt and hash:
+    const hash = bcrypt.hashSync(password, 10)
+
+    return hash
+}
+
+function verifyPassword(password, hash) {
+    // Require bcrypt
+    const bcrypt = dcodeIO.bcrypt;
+
+    return bcrypt.compareSync(password, hash)
+}
+
+function activeNavBtn(target) {
     // Reset element current 'active' class 
-    navLiElems.forEach(li => li.classList.remove('active'))
+    navLiElems.forEach(link => link.parentElement.classList.remove('active'))
 
     // Return if e isn't defined
-    if (e == undefined) return
+    if (target == undefined || target == null) return
 
-    // Add class 'active' to the current elem
-    if (e.parentElement.tagName == 'LI') {
-        e.parentElement.classList.add('active')
-    } else {
-        // When on home page
-        if (e.textContent == 'Sign Up') {
-            navLiElems.forEach(li => li.textContent == 'Sign Up' && li.classList.add('active'))
-        }
-        if (e.textContent == 'Log In') {
-            navLiElems.forEach(li => li.textContent == 'Log In' && li.classList.add('active'))
-        }
+    // Add class 'active' to the parent's target elem
+    if (target == 'signup') {
+        navLiElems.forEach(link => link.textContent == 'Sign Up' && link.parentElement.classList.add('active'))
+    }
+    if (target == 'login') {
+        navLiElems.forEach(link => link.textContent == 'Log In' && link.parentElement.classList.add('active'))
+    }
+    if (target == 'dashboard') {
+        navLiElems.forEach(link => link.textContent == 'Dashboard' && link.parentElement.classList.add('active'))
+    }
+    if (target == 'account') {
+        navLiElems.forEach(link => link.textContent == 'Account Settings' && link.parentElement.classList.add('active'))
     }
 }
 
-function showHomeSection(add) {
-    if (add) {
-        sectionHome.classList.add('hidden')
-    } else {
-        sectionHome.classList.remove('hidden')
-    }
+function showHomeSection() {
+    sectionHome.classList.remove('hidden')
 }
 
-function showSignUpSection(add) {
-    if (add) {
-        sectionSignUp.classList.add('hidden')
-    } else {
-        sectionSignUp.classList.remove('hidden')
-    }
+function showSignUpSection() {
+    sectionSignUp.classList.remove('hidden')
 }
 
-function showLogInSection(add) {
-    if (add) {
-        sectionLogIn.classList.add('hidden')
-    } else {
-        sectionLogIn.classList.remove('hidden')
-    }
+function showLogInSection() {
+    sectionLogIn.classList.remove('hidden')
 }
 
-function showAccountSection(add) {
-    if (add) {
-        sectionAccount.classList.add('hidden')
-    } else {
-        sectionAccount.classList.remove('hidden')
-    }
+function showAccountSection() {
+    sectionAccount.classList.remove('hidden')
 }
 
-function showDashboardSection(add) {
-    if (add) {
-        sectionDashboard.classList.add('hidden')
-    } else {
-        sectionDashboard.classList.remove('hidden')
-    }
+function showDashboardSection() {
+    sectionDashboard.classList.remove('hidden')
 }
 
 function hideAllSections() {
@@ -179,6 +326,12 @@ function hideAllSections() {
 function logUserIn() {
     loggedOutNavLinks.forEach(section => section.classList.add('hidden'))
     loggedInNavLinks.forEach(section => section.classList.remove('hidden'))
+
+    // Hide all sections
+    hideAllSections()
+
+    // Show the dashboard
+    showDashboardSection()
 }
 
 function logUserOut() {
